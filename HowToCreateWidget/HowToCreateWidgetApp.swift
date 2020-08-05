1785818822e7b54b572801c6473686fab162cd14
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 13:46:14 +0300

    Initial Commit

---
+++ HowToCreateWidget/HowToCreateWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateWidgetApp.swift
+//  HowToCreateWidget
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
